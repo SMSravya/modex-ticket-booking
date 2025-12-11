f65841e56591d111fddb5fb49bc383e85d285905
SravyaiBee <sravya@ibeeanalytics.com>
2025-12-12 01:38:53 +0530

    Fix FormEvent type import

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useEffect, useState } from "react";
+import type { FormEvent } from "react";
+import { useEffect, useState } from "react";
 import type { Booking, Show } from "../api";
 import { createShow, fetchAdminBookings } from "../api";
 import { useAppContext } from "../context/AppContext";
